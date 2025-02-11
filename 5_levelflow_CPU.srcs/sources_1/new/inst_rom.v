dd219773df5d131dc135559442f20d833975c372
Volodya-lc <3354405160@qq.com>
2025-02-11 12:57:27 +0800

    Add files via upload

---
+++ 5_levelflow_CPU.srcs/sources_1/new/inst_rom.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+`include "defines.v"
+
+module inst_rom(
+
+//	input wire clk,
+	input wire ce,//使能信号
+	input wire [31:0] addr,//要读取的指令地址
+	output reg [31:0] inst//读出的指令
+	
+);
+	reg[31:0]  inst_mem[0:131071-1];
+
+	
+ initial begin  
+   $readmemh("D:\\inst_rom.data",inst_mem);
+    end 
+	always @ (*) begin
+		if (ce == 1'b0) begin
+			inst <= 32'h00000000;
+	  end else begin
+		  inst <= inst_mem[addr[17+1:2]];
+		end
+	end
+
+endmodule
